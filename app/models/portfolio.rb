8b2c811879499806b52392716771c2e98d6c0ec6
vonxar <vonxar@gamil.com>
2020-07-01 02:12:23 +0000

    評価

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -24,18 +24,21 @@ class Portfolio < ApplicationRecord
   #has_one_attached :body
    
   attachment :image
-  #ソート
+  #ソート 
   def self.sort(selection)
     case selection
     when 'new'
       return all.order(created_at: :DESC)
     when 'old'
       return all.order(created_at: :ASC)
-    when 'likes'
+    when 'likes' #Like.group(:portfolio_id)で、post_idが同じ投稿をグループ分けします。 
+    #order(Arel.sql('count(portfolio_id) desc'))で、グループ分けされた投稿へどれだけuser_idが格納されているかをカウントして多いものから並べます（いいねされているかを集計してます）。
       return find(Like.group(:portfolio_id).order(Arel.sql('count(portfolio_id) desc')).pluck(:portfolio_id))
+      #pluck(:portfolio_id)では、portfolio_idそのものの取得を行っています。これを記述しないと、portfolioへ対して呼び出しているfindに該当するものが見つからず、エラーになってしまいます。
     when 'dislikes'
       return find(Like.group(:portfolio_id).order(Arel.sql('count(portfolio_id) asc')).pluck(:portfolio_id))
+    when 'visit'
+       return find(Impression.group(:impressionable_id).order('count(impressionable_id) desc').limit(3).pluck(:impressionable_id))
     end
   end
-  
 end
