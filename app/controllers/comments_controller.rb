df2100586b952deabf375d0dd3faee7ca9c53f6f
vonxar <62320489+vonxar@users.noreply.github.com>
2020-07-07 13:52:50 +0900

    Revert "Revert "Allfix""

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,12 @@ class CommentsController < ApplicationController
     @portfolio = Portfolio.find(params[:portfolio_id])
     @comment = current_user.comments.new(comment_params)
     @comment.portfolio_id = @portfolio.id
-    @comment.save
+    if @comment.save
+    # @comment_portfolio = @comment.portfolio
+    #投稿に紐づいたコメントを作成
+      #通知の作成
+      @comment.portfolio.create_notification_comment!(current_user, @comment.id)
+    end
   end
   
   def destroy
@@ -13,9 +18,11 @@ class CommentsController < ApplicationController
     Comment.find_by(id: params[:id], portfolio_id: params[:portfolio_id]).destroy
   end
   
+  
   private
   
   def comment_params
     params.require(:comment).permit(:comment,:rate)
   end
+  
 end
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,6 +4,12 @@ class LikesController <  ApplicationController
   def create
     @like = current_user.likes.create(portfolio_id: params[:portfolio_id])
     @portfolio = Portfolio.find(params[:portfolio_id])
+    #通知の作成
+     @portfolio.create_notification_by(current_user)
+      respond_to do |format|
+        format.html {redirect_to request.referrer}
+        format.js
+      end
   end
   
   def destroy
