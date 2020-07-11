bf948449f4a3f56f38bc606c6b40ad0147a35563
vonxar <vonxar@gamil.com>
2020-07-11 07:31:05 +0000

    修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.14.1"
 
 set :application, "PortaAperta"
-set :repo_url, "git@github.com:vonxar/PortaAperta.git"
+set :repo_url, "https://github.com/vonxar/PortaAperta.git"
 set :deploy_to, "/home/ec2-user/PortaAperta"
 set :rbenv_ruby, '2.6.3'
 set :linked_files, %w{config/master.key .env}
